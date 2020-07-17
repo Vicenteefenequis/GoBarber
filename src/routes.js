d1707dab37ea5ec92610560fe788316fd52d3f95
Vicenteefenequis <efenequis19981@gmail.com>
2020-07-17 17:40:21 -0300

    Listing of service providers

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,10 @@ import multerConfig from './config/multer'
 
 import UserController from './app/controllers/UserController'
 import SessionControler from './app/controllers/SessionController'
-import authMiddleware from './app/middlewares/auth'
+import FileController from './app/controllers/FileController'
+import ProviderController from './app/controllers/ProviderController'
 
+import authMiddleware from './app/middlewares/auth'
 const routes = new Router()
 const upload = multer(multerConfig)
 
@@ -16,8 +18,8 @@ routes.use(authMiddleware)
 
 routes.put('/users', UserController.update)
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true })
-})
+routes.get('/providers', ProviderController.index)
+
+routes.post('/files', upload.single('file'), FileController.store)
 
 export default routes
